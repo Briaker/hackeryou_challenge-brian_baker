623e746d46a5eeda0df4332bfab970450e494875
Briaker <mail@brianbaker.ca>
2016-12-24 14:36:41 -0500

    Cleaned up code formatting

--- src/js/menus.js
+++ src/js/menus.js
@@ -1,22 +1,15 @@
 $(document).ready(function() {
 
 	$(".menu-toggle").click(function() {
-		var menu = $(this).parent().siblings(".menu");
 		var menuToggle = $(this);
-		var label = $(this).siblings(".menu-label");
+		var label = menuToggle.siblings(".menu-label");
+		var menu = menuToggle.parent().siblings(".menu");
+
 		if (menu) {
-			// console.log("Menu was clicked");
 			menu.toggleClass("menu-opened");
 			menuToggle.toggleClass("menu-opened");
 			label.toggleClass("menu-opened");
-			// if(list.hasClass("listClosed")) {
-			// 	list.removeClass("listClosed");
-			// 	$(this).removeClass("hide");
-			// }
-			// else {
-			// 	list.addClass("listClosed");
-			// 	$(this).addClass("hide");
-			// }
 		}
 	});
+
 });
